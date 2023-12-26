ab3f27362770de7f7feb5662cc47be882cf2adaa
Dmitrii Kobozev <dmitrii_kobozev@Noutbuk-Dmitrii.local>
2023-12-27 04:01:29 +0600

    added ssh connection and tests for ssh

--- Sem2/Task1/conftest.py
+++ Sem2/Task1/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from checkers import checkout, getout
+from ssh_checkers import ssh_checkout_get, ssh_checkout, upload_files
 import yaml
 import random, string
 from datetime import datetime
@@ -8,54 +9,133 @@ with open('/Users/dmitrii_kobozev/Desktop/Python_autotests/Linux_AutoTest/Sem2/T
     data = yaml.safe_load(f)
 
 # создание папок для тестовых данных
-@pytest.fixture()
+@pytest.fixture(autouse=True, scope='module')
 def make_folders():
-    return checkout('mkdri {} {} {} {}'.format(data["folder_in"], data["folder_out1"],data["folder_out2"],data["folder_out3"]), '')
+    return ssh_checkout(f"{data['ip']}",
+                        f"{data['user']}",
+                        f"{data['password']}",
+                        f"mkdir -p {data['folder_in']} {data['folder_out1']} "
+                        f"{data['folder_out2']} {data['folder_out3']}",
+                        "")
 
 # очистка папок с тестовыми данными после тестов
 @pytest.fixture()
 def clear_folders():
-    return checkout('rm -rf {}/* {}/* {}/* {}/*'.format(data["folder_in"], data["folder_out1"],data["folder_out2"],data["folder_out3"]), '')
+    return ssh_checkout(f"{data['ip']}",
+                        f"{data['user']}",
+                        f"{data['password']}",
+                        'rm -rf {}/* {}/* {}/* {}/*'.format(data['folder_in'], data['folder_out1'],
+                                                            data['folder_out2'], data['folder_out3']),
+                        "")
 
 # создание тестовых файлов
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def make_files():
     list_of_files = []
-    for i in range(data["count_test_files"]):
-        filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k = data["length_name_file"]))
-        if checkout('cd {}; dd if=/dev/urandom of={} bs={} count=1 iflag=fullblock'.format(data["folder_in"], filename, data["bs"]),''):
+    for i in range(data['count']):
+        filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=data["length_name_file"]))
+        if ssh_checkout(f"{data['ip']}",
+                        f"{data['user']}",
+                        f"{data['password']}",
+                        f"cd {data['folder_in']}; dd if=/dev/urandom of={filename} b"
+                        f"s={data['bs']} count=1 iflag=fullblock",
+                        ''):
             list_of_files.append(filename)
     return list_of_files
 
 # создание подпапки и файла в нем
 @pytest.fixture()
 def make_subfolder():
-    testfile_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
-    subfolder_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
-    if not checkout('cd {}; mkdir {}'.format(data["folder_in"], subfolder_name), ''):
+    testfile_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k = data["length_name_file"]))
+    subfolder_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k = data["length_name_file"]))
+    if not ssh_checkout(f"{data['ip']}",
+                        f"{data['user']}", f"{data['password']}",
+                        f"cd {data['folder_in']}; mkdir {subfolder_name} ",
+                        ''):
         return None, None
-    if not checkout('cd {}/{}; dd if=/dev/urandom of={} bs={} count=1 iflag=fullblock'.format(data["folder_in"], subfolder_name, 
-                                                                                          testfile_name, data["bs"]),''):
+    if not ssh_checkout(f"{data['ip']}",
+                        f"{data['user']}",
+                        f"{data['password']}",
+                        f"cd {data['folder_in']}/{subfolder_name};"
+                        f" dd if=/dev/urandom of={testfile_name} bs={data['bs']} count=1 iflag=fullblock",
+                        ''):
         return subfolder_name, None
     else:
         return subfolder_name, testfile_name
     
+
+@pytest.fixture(autouse=True, scope='module')
+def deploy():
+    res = []
+    upload_files(f"{data['ip']}",
+                 f"{data['user']}",
+                 f"{data['password']}",
+                 '/home/user/p7zip-full.deb',
+                 '/home/user2/p7zip-full.deb')
+    res.append(ssh_checkout(f"{data['ip']}",
+                            f"{data['user']}",
+                            f"{data['password']}",
+                            'echo "1111" | sudo -S dpkg -i /home/user2/p7zip-full.deb',
+                            'Настраивается пакет'))
+    res.append(ssh_checkout(f"{data['ip']}",
+                            f"{data['user']}",
+                            f"{data['password']}",
+                            'echo "1111" | sudo -S dpkg -s p7zip-full',
+                            'Status: install ok installed'))
+    return all(res)
+
+
+# создание плохих архивов
 @pytest.fixture()
 def make_bad_arx():
-    checkout(f"cd {data['folder_in']}; 7z a -t{data['arc_type']}{data['folder_out']}/bad_arx",
-             "Everything is Ok")
-    checkout(f"truncate -s 1 {data['folder_out']}/bad_arx.{data['arc_type']}", "")
+    ssh_checkout(f"{data['ip']}",
+                 f"{data['user']}",
+                 f"{data['password']}",
+                 f"cd {data['folder_in']}; 7z a {data['folder_out']}/bad_arx -t{data['type']}",
+                 "Everything is Ok")
+    ssh_checkout(f"{data['ip']}",
+                 f"{data['user']}",
+                 f"{data['password']}",
+                 f"truncate -s 1 {data['folder_out']}/bad_arx.{data['type']}",
+                 "")
 
+# печать времени
 @pytest.fixture(autouse=True)
 def print_time():
     print(f'Start: {datetime.now().strftime("%H:%M:%s.%f")}')
     yield
     print(f'\nFinish: {datetime.now().strftime("%H:%M:%s.%f")}')
 
+# простые статы
 @pytest.fixture(autouse=True)
 def stat_log():
     yield
     time = datetime.now().strftime("%H:%M:%s.%f")
-    stat = getout('sysctl -n vm.loadavg')
-    checkout(f"echo 'time:{time} count:{data['count_test_files']} size;{data['bs']} stat:{stat}' >> {data['stat_file_path']}", '')
-    
+    stat = ssh_checkout_get(f"{data['ip']}",
+                            f"{data['user']}",
+                            f"{data['password']}",
+                            'cat /proc/loadavg')     ## тут линукс команда, так как мы уже зашли на удаленную машину
+    ssh_checkout(f"{data['ip']}",
+                 f"{data['user']}",
+                 f"{data['password']}",
+                 f"echo 'time:{time} count:{data['count']} size;{data['bs']} stat:{stat}' >> {data['stat_ssh_file_path']}",
+                 '')
+
+
+
+# время начала
+@pytest.fixture()
+def start_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# расширеные статы
+@pytest.fixture(autouse=True)
+def stat(start_time):
+    yield
+    stat = ssh_checkout_get(f"{data['ip']}",
+                            f"{data['user']}",
+                            f"{data['password']}",'cat /proc/loadavg')   ## тут линукс команда, так как мы уже зашли на удаленную машину
+    log_load = (f"Start: {start_time}, time: {datetime.now().strftime('%H:%M:%S')},"
+                f" count:{data['count']} size;{data['bs']} stat:{stat} \n Finish: {stat}")
+    with open('stat', 'a') as f:
+        f.write(log_load + "\n")
--- Sem2/Task1/test_negative.py
+++ Sem2/Task1/test_negative.py
@@ -1,23 +1,23 @@
-from checkers import checkoutneg
 from logging_fucn import log_neg_step_info
 import yaml
+from ssh_checkers import ssh_checkout_negative
+from messages import error_message
 
 with open('/Users/dmitrii_kobozev/Desktop/Python_autotests/Linux_AutoTest/Sem2/Task1/config.yaml') as f:
     data = yaml.safe_load(f)
 
+class TestNegative:
+    def test_negstep1(self, make_bad_arx):
+        STEP = 1
+        log_neg_step_info(STEP)
+        res = ssh_checkout_negative(f"{data['ip']}", f"{data['user']}", f"{data['password']}",
+                                        f"cd {data['folder_out1']}; 7zz e bad_arx.{data['arc_type']} -o{data['folder_out2']} -y",
+                                        "ERRORS")
+        assert res, error_message(STEP)
 
-def test_negstep1():
-    STEP = 1
-    log_neg_step_info(STEP)
-    assert checkoutneg(f'cd {data["folder_out1"]}; 7zz e bad_arx.{data["arc_type"]} -o{data["folder_out2"]} -y', 'ERROR')
-
-def test_negstep2():
-    STEP = 2
-    log_neg_step_info(STEP)
-    assert checkoutneg(f'cd {data["folder_out1"]}; 7zz t bad_arx.{data["arc_type"]}', 'ERROR')
-
-
-
-if __name__ == '__main__':
-    test_negstep1()
-    test_negstep2()
+    def test_negstep2(self):
+        STEP = 2
+        log_neg_step_info(STEP)
+        res = ssh_checkout_negative(f"{data['ip']}", f"{data['user']}", f"{data['password']}",
+                                     f"cd {data['folder_out1']}; 7z t bad_arx.{data['arc_type']}",  "ERRORS"), 'test2 FAIL'
+        assert res, error_message(STEP)
